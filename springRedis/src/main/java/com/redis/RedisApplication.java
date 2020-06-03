6755811b21546c7b2884dc206c6913f872328662
20781 <2078199832@qq.com>
2020-06-03 18:26:46 +0800

    springboot整合kafka

--- springRedis/src/main/java/com/redis/RedisApplication.java
+++ springRedis/src/main/java/com/redis/RedisApplication.java
@@ -1,11 +1,16 @@
 package com.redis;
 
 
+import com.redis.dao.UserRedis;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedisApplication {
+
+    @Autowired
+    private static UserRedis userRedis;
 
     public static void main(String[] args) {
          SpringApplication.run(RedisApplication.class, args);
